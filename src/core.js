d15832635c419059a0eb5fef302b93bfed853b1a
Matthew Williams <m.williams@me.com>
2011-04-06 14:45:55 +0100

    Fixed beta.cdf. Allowed some functions to run on jstat objects

--- src/core.js
+++ src/core.js
@@ -421,6 +421,15 @@ jstat.extend({
 		return jstat( res );
 	},
 
+	// generate sequence
+	seq : function( min, max, length, func ) {
+		var arr = [],
+			step = ( max - min ) / ( length - 1 );
+		// TODO: replace toFixed value to user configurable parameter
+		for ( ; min <= max; min += step ) arr.push(+( func ? func.call( null, min ) : min ).toFixed(6));
+		return jstat( arr );
+	},
+
 	// factorial of n
 	factorial : function( n ) {
 		var fval = 1;
@@ -444,6 +453,12 @@ jstat.extend({
 
 	// gamma of x
 	gamma : function( x ) {
+
+		if( isNaN( x ) ) {
+			// run for all values in matrix
+			return x.map( function( value ) { return jstat.gamma( value ) } );
+		}
+
 		var v = 1,
 			w;
 		if ( x == Math.floor( x ) ) return jstat.factorial( x - 1 );
@@ -456,22 +471,34 @@ jstat.extend({
 	},
 
 	// Log-gamma function
-	gammaln : function( xx ) {
-		var x, y, tmp, ser,j = 0,
+	gammaln : function( x ) {
+
+		if( isNaN( x ) ) {
+			// run for all values in matrix
+			return x.map( function( value ) { return jstat.gammaln( value ) } );
+		}
+
+		var xx, y, tmp, ser,j = 0,
 			cof = [76.18009172947146, -86.50532032941677, 24.01409824083091,
 				-1.231739572450155, 0.1208650973866179e-2, -0.5395239384953e-5];
 
-		y = x = xx;
-		tmp = x + 5.5;
-		tmp -= ( x + 0.5 ) * Math.log( tmp );
+		y = xx = x;
+		tmp = xx + 5.5;
+		tmp -= ( xx + 0.5 ) * Math.log( tmp );
 		ser = 1.000000000190015;
 		for( ; j < 5; j++ ) ser += cof[j] / ++y;
 
-		return -tmp + Math.log( 2.5066282746310005 * ser / x);
+		return -tmp + Math.log( 2.5066282746310005 * ser / xx);
 	},
 
 	// lower incomplete gamma function
 	lgamma : function( x, s, dt ) {
+
+		if( isNaN( x ) ) {
+			// run for all values in matrix
+			return x.map( function( value ) { return jstat.lgamma( value, s, dt ) } );
+		}
+
 		dt = dt || 0.1;
 		var sum = 0,
 			i = 0;
@@ -490,14 +517,7 @@ jstat.extend({
 		return sum;
 	},
 
-	// generate sequence
-	seq : function( min, max, length, func ) {
-		var arr = [],
-			step = ( max - min ) / ( length - 1 );
-		// TODO: replace toFixed value to user configurable parameter
-		for ( ; min <= max; min += step ) arr.push(+( func ? func.call( null, min ) : min ).toFixed(6));
-		return arr;
-	},
+	
 
 	// vector/matrix specific functionality //
 
@@ -707,7 +727,7 @@ jstat.extend({
 
 		if( x < ( a + 1 ) / ( a + b + 2 ) )
 			// Use continued fraction directly.
-			return bt * betacf( x, b, a ) / a;
+			return bt * betacf( x, a, b ) / a;
 
 		// else use continued fraction after making the symmetry transformation.
 		return 1 - bt * betacf( 1 - x, b, a ) / b;
@@ -730,29 +750,8 @@ jstat.extend({
 		},
 
 		// cumulative beta distribution
-		// BUG: returns Infinity when a < 1
-		cdf : function( x, alpha, beta, dt ) {
-			var ab1 = alpha + beta - 1,
-				fact = jstat.factorial,
-				gamma = jstat.gamma,
-				ab1fact = fact( ab1 ),
-				j = alpha,
-				sum = 0;
-
-			// quick calculation if a and b are integers
-			if ( alpha === Math.floor( alpha ) && beta === Math.floor( beta )) {
-				for ( ; j <= ab1; j++ ) {
-					sum += ab1fact / ( fact( j ) * fact( ab1 - j )) * Math.pow( x, j ) * Math.pow( 1 - x, ab1 - j );
-				};
-				return sum;
-			};
-
-			// otherwise will have to integrate
-			dt = dt || 0.1;
-			for ( j = 0.1; j <= x; j += dt ) {
-				sum += jstat.beta( j, alpha, beta ) * dt;
-			};
-			return sum;
+		cdf : function( x, alpha, beta ) {
+			return jstat.incompleteBeta( x, alpha, beta );
 		},
 
 		mean : function( alpha, beta ) {
