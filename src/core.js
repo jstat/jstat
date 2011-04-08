2d6b7c7611fcb1f93583ba9b3b4d92dc2a1755c8
Matthew Williams <m.williams@me.com>
2011-04-08 14:55:05 +0100

    Added inverse incomplete gamma function gammapInv( p, a )

--- src/core.js
+++ src/core.js
@@ -563,6 +563,41 @@ jStat.extend({
 		}
 	},
 
+	// Returns the inverse incomplte gamma function
+	gammapInv : function( p, a ) {
+		var j = 0,x,gln,err,t,u,pp,lna1,afac,a1=a-1,EPS=1e-8;
+		gln = j$.gammaln( a );
+
+		if( a <= 0 ) xerror();
+		if( p >= 1 ) return Math.max( 100, a + 100 * Math.sqrt( a ) );
+		if( p <= 0 ) return 0;
+		if( a > 1 ) {
+			lna1 = Math.log( a1 );
+			afac = Math.exp( a1 * ( lna1 - 1 ) - gln );
+			pp = ( p < 0.5 ) ? p : 1 - p;
+			t = Math.sqrt( -2 * Math.log( pp ) );
+			x = ( 2.30753 + t * 0.27061 ) / ( 1 + t * ( 0.99229 + t * 0.04481 ) );
+			if( p < 0.5 ) x = -x;
+			x = Math.max( 1e-3, a * Math.pow( 1 - 1 / ( 9 * a ) - x / ( 3 * Math.sqrt( a ) ), 3 ) );
+		} else {
+			t = 1 - a * ( 0.253 + a * 0.12 );
+			if( p < t ) x = Math.pow( p / t, 1 / a);
+			else x = 1 -Math.log( 1 - ( p - t ) / ( 1 - t ) );
+		}
+
+		for( ; j < 12; j++ ) {
+			if( x <= 0 ) return 0;
+			err = j$.gammap( x, a ) - p;
+			if( a > 1 ) t = afac * Math.exp( -( x - a1 ) + a1 * ( Math.log( x ) - lna1 ) );
+			else t = Math.exp( -x + a1 * Math.log( x ) - gln );
+			u = err / t;
+			x -= ( t = u / ( 1 - 0.5 * Math.min(1, u * ( ( a - 1 ) / x - 1 ) ) ) );
+			if( x <= 0 ) x = 0.5 * ( x + t );
+			if( Math.abs( t ) < EPS * x ) break;
+		}
+		return x;
+	},
+
 	// Returns the error function erf(x)
 	erf : function( x ) {
 		if( isNaN( x ) ) {
