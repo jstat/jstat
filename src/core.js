96602a84782d511470244fa6acfd2285ddfcb319
Matthew Williams <m.williams@me.com>
2011-04-08 12:19:34 +0100

    Optimized error function, added complementary error function (1 - erf(x))

--- src/core.js
+++ src/core.js
@@ -569,10 +569,66 @@ jStat.extend({
 			// run for all values in matrix
 			return x.map( function( value ) { return jStat.erf( value ); } );
 		}
-		return ( x < 0.0 ) ? -jStat.gammap( x * x, 0.5 ) : jStat.gammap( x * x, 0.5 );
+
+		var cof = [
+			-1.3026537197817094,
+			6.4196979235649026e-1,
+			1.9476473204185836e-2,
+			-9.561514786808631e-3,
+			-9.46595344482036e-4,
+			3.66839497852761e-4,
+			4.2523324806907e-5,
+			-2.0278578112534e-5,
+			-1.624290004647e-6,
+			1.303655835580e-6,
+			1.5626441722e-8,
+			-8.5238095915e-8,
+			6.529054439e-9,
+			5.059343495e-9,
+			-9.91364156e-10,
+			-2.27365122e-10,
+			9.6467911e-11,
+			2.394038e-12,
+			-6.886027e-12,
+			8.94487e-13,
+			3.13092e-13,
+			-1.12708e-13,
+			3.81e-16,
+			7.106e-15,
+			-1.523e-15,
+			-9.4e-17,
+			1.21e-16,
+			-2.8e-17
+		],
+		j = cof.length - 1, t, ty, tmp, d = 0, dd = 0,res,isneg = false;
+
+		if( x < 0 ) {
+			x = -x;
+			isneg = true;
+		}
+
+		t = 2 / ( 2 + x );
+		ty = 4 * t - 2;
+		for( ; j > 0; j-- ) {
+			tmp = d;
+			d = ty * d - dd + cof[j];
+			dd = tmp;
+		}
+
+		res = t * Math.exp( -x*x + 0.5 * ( cof[0] + ty * d ) - dd );
+		return ( isneg ) ? res - 1 : 1 - res;
 	},
 
+	// Returns the complmentary error function erfc(x)
+	erfc : function( x ) {
+		if( isNaN( x ) ) {
+			// run for all values in matrix
+			return x.map( function( value ) { return jStat.erfc( value ); } );
+		}
 
+		return 1 - j$.erf( x );
+	},
+	
 	// vector/matrix specific functionality //
 
 	// sum of an array
