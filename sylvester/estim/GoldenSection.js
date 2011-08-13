8899aae0389c62f23901fb748cfdbc7b375cc521
Florian Pons <florian.pons@gmail.com>
2011-08-13 18:49:01 +0200

    Change licence from Cecill to MIT (then i use the same licence as Sylvester for matrix and jStat for stat)

--- sylvester/estim/GoldenSection.js
+++ sylvester/estim/GoldenSection.js
@@ -1,16 +1,6 @@
 // Copyright 2011 Florian Pons (florian.pons.tv). All Rights Reserved.
 //
-// Licensed under the Cecill License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS-IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Licensed under the MIT License
 
 /**
  * @fileoverview Classe d'implementation de la méthode Golden Section de recherche de minimal.
