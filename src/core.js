fc29c1a710ff0b22f3940000112ed12c3f2a129e
tushar <tushar@tushar-Studio-1555.(none)>
2011-08-25 12:33:45 +0530

    core.js cleaned up

--- src/core.js
+++ src/core.js
@@ -6,18 +6,15 @@
 this.j$ = this.jStat = (function( Math, undefined ) {
 
 	// for quick reference
-	var slice = Array.prototype.slice,
+var slice = Array.prototype.slice,
 	toString = Object.prototype.toString,
-
-	// for code compression
-	length = 'length',
 
 	// ascending/descending functions for sort
 	ascNum = function( a, b ) { return a - b; },
 	descNum = function( a, b ) { return b - a; },
 
 	// test if array
-	isArray = function( arg ) {
+	isArray = Array.isArray || function( arg ) {
 		return toString.call( arg ) === "[object Array]";
 	},
 
@@ -29,12 +26,35 @@ this.j$ = this.jStat = (function( Math, undefined ) {
 	// test if object
 	isObject = function( arg ) {
 		return toString.call( arg ) === "[object Object]";
+	},
+
+	// calculate correction for IEEE
+	calcRdx = function( n, m ) {
+		var val = n > m ? n : m;
+		return Math.pow( 10, 15 - ~~( Math.log((( val > 0 ) ? val : -val )) * Math.LOG10E ));
 	};
+
+// implement bind if browser doesn't natively support it
+if ( !Function.prototype.bind ) {
+	Function.prototype.bind = function( obj ) {
+		var slice = [].slice,
+			args = slice.call( arguments, 1 ),
+			self = this,
+			nop = function() {},
+			bound = function() {
+				return self.apply( this instanceof nop ? this : ( obj || {} ),
+					args.concat( slice.call( arguments ))
+				);
+			};
+		bound.prototype = this.prototype;
+		return bound;
+	};
+}
 
 // global function
 function jStat() {
-	return new jStat.fn.init( slice.call( arguments ));
-};
+	return new jStat.fn.init( arguments );
+}
 
 // extend jStat prototype
 jStat.fn = jStat.prototype = {
@@ -44,16 +64,19 @@ jStat.fn = jStat.prototype = {
 		// if first argument is an array, must be vector or matrix
 		if ( isArray( args[0] )) {
 			if ( isArray( args[0][0] )) {
-				for ( var i = 0; i < args[0][length]; i++ ) {
-					this.push( args[0][i] );
+				for ( var i = 0; i < args[0].length; i++ ) {
+					this[i] = args[0][i];
 				}
+				this.length = args[0].length;
 			} else {
-				this.push( args[0] );
+				this[0] = args[0];
+				this.length = 1;
 			}
 
 		// if first argument is number, assume creation of sequence
 		} else if ( !isNaN( args[0] )) {
-			this.push( jStat.seq.apply( null, args )[0] );
+			this[0] = jStat.seq.apply( null, args );
+			this.length = 1;
 		}
 		return this;
 	},
@@ -63,11 +86,11 @@ jStat.fn = jStat.prototype = {
 
 	// return clean array
 	toArray : function() {
-		return slice.call( this, 0 );
+		return slice.call( this );
 	},
 
 	// only to be used internally
-	push : Array.prototype.push,
+	push : [].push,
 	sort : [].sort,
 	splice : [].splice
 };
@@ -76,14 +99,14 @@ jStat.fn.init.prototype = jStat.fn;
 // create method for easy extension
 jStat.extend = function( obj ) {
 	var args = slice.call( arguments ),
-	i = 1, j;
-	if ( args[length] === 1 ) {
+		i = 1, j;
+	if ( args.length === 1 ) {
 		for ( i in obj ) {
 			jStat[i] = obj[i];
 		}
 		return this;
 	}
-	for ( ; i < args[length]; i++ ) {
+	for ( ; i < args.length; i++ ) {
 		for ( j in args[i] ) obj[j] = args[i][j];
 	}
 	return obj;
@@ -91,34 +114,47 @@ jStat.extend = function( obj ) {
 
 // extend jStat.fn with methods which don't require arguments and work on columns
 (function( funcs ) {
-	for ( var i = 0; i < funcs[length]; i++ ) (function( passfunc ) {
+	for ( var i = 0; i < funcs.length; i++ ) (function( passfunc ) {
 
 		// if a matrix is passed, automatically assume operation should be done on the columns
-		jStat.fn[ passfunc ] = function( fullbool ) {
+		jStat.fn[ passfunc ] = function( fullbool, func ) {
 			var arr = [],
 				i = 0,
-				tmpthis;
-
-			if ( this[length] > 1 ) {
-				tmpthis = this.transpose();
-				for ( ; i < tmpthis[length]; i++ ) {
-					arr[i] = jStat[ passfunc ]( tmpthis[i] );
-				}
-				if ( fullbool === true ) {
-					arr = jStat[ passfunc ]( arr );
-				}
+				tmpthis = this;
+			if ( isFunction( fullbool )) {
+				func = fullbool;
+				fullbool = false;
 			}
-
-			return arr[length] > 1 ? jStat( arr ) : jStat[ passfunc ]( this[0] );
+			if ( func ) {
+				setTimeout( function() {
+					func.call( tmpthis, jStat.fn[ passfunc ].call( tmpthis, fullbool ));
+				}, 15 );
+				return this;
+			}
+			if ( this.length > 1 ) {
+				tmpthis = fullbool === true ? this : this.transpose();
+				for ( ; i < tmpthis.length; i++ )
+					arr[i] = jStat[ passfunc ]( tmpthis[i] );
+				arr = fullbool === true ? jStat[ passfunc ]( arr ) : arr;
+			}
+			return arr;
 		};
 	})( funcs[i] );
 })( 'sum min max mean median mode range variance stdev meandev meddev quartiles'.split( ' ' ));
 
 // extend jStat.fn with methods that have no argument
 (function( funcs ) {
-	for ( var i = 0; i < funcs[length]; i++ ) (function( passfunc ) {
-		jStat.fn[ passfunc ] = function() {
-			var results = jStat[ passfunc ]( this );
+	for ( var i = 0; i < funcs.length; i++ ) (function( passfunc ) {
+		jStat.fn[ passfunc ] = function( func ) {
+			var tmpthis = this,
+				results;
+			if ( func ) {
+				setTimeout( function() {
+					func.call( tmpthis, jStat.fn[ passfunc ].call( tmpthis ));
+				}, 15 );
+				return this;
+			}
+			results = jStat[ passfunc ]( this );
 			return isArray( results ) ? jStat( results ) : results;
 		};
 	})( funcs[i] );
@@ -126,32 +162,31 @@ jStat.extend = function( obj ) {
 
 // extend jStat.fn with methods that require one argument
 (function( funcs ) {
-	for ( var i = 0; i < funcs[length]; i++ ) (function( passfunc ) {
-		jStat.fn[ passfunc ] = function( arg ) {
+	for ( var i = 0; i < funcs.length; i++ ) (function( passfunc ) {
+		jStat.fn[ passfunc ] = function( arg, func ) {
+			var tmpthis = this;
+			if ( func ) {
+				setTimeout( function() {
+					func.call( tmpthis, jStat.fn[ passfunc ].call( tmpthis, arg ));
+				}, 15 );
+				return this;
+			}
 			return jStat( jStat[ passfunc ]( this, arg ));
 		};
 	})( funcs[i] );
-})( 'add divide multiply subtract dot pow abs angle sq vec_transpose'.split( ' ' ));
-
-(function( funcs ) {
-	for ( var i = 0; i < funcs[length]; i++ ) (function( passfunc ) {
-		jStat.fn[ passfunc ] = function( arg1, arg2 ) {
-			return jStat( jStat[ passfunc ]( arg1, arg2 ));
-		};
-	})( funcs[i] );
-})( 'vec_mult vec_sub'.split( ' ' ));
+})( 'add divide multiply subtract dot pow abs angle'.split( ' ' ));
 
 // extend jStat.fn
 jStat.extend( jStat.fn, {
 
 	// Returns the number of rows in the matrix
 	rows: function() {
-		return this[length] || 1;
+		return this.length || 1;
 	},
 
 	// Returns the number of columns in the matrix
 	cols: function() {
-		return this[0][length] || 1;
+		return this[0].length || 1;
 	},
 
 	// Returns the dimensions of the object { rows: i, cols: j }
@@ -164,17 +199,17 @@ jStat.extend( jStat.fn, {
 
 	// Returns a specified row as a vector
 	row: function( index ) {
-	    return jStat( this[index] );
+		return jStat( this[index] );
 	},
 
 	// Returns the specified column as a vector
 	col: function( index ) {
-	    var column = [],
+		var column = [],
 			i = 0;
-	    for ( ; i < this[length]; i++ ) {
+		for ( ; i < this.length; i++ ) {
 			column[i] = [ this[i][index] ];
-	    }
-	    return jStat( column );
+		}
+		return jStat( column );
 	},
 
 	// Returns the diagonal of the matrix
@@ -209,21 +244,26 @@ jStat.extend( jStat.fn, {
 		jStat.alter( this, func );
 		return this;
 	},
-	      
+
 	max: function() { 
+	  var row_cnt = 0, col_cnt = 0, value,
+	  row_max = this.rows(),
+	  col_max = this.cols(),
 	  max = null;
-	  for ( var row_cnt = 0 ; row_cnt < this.rows(); row_cnt++) {
-	    for ( var col_cnt = 0; col_cnt < this.cols(); col_cnt++) {
-	      if( ! isNaN (this[row_cnt][col_cnt]) && this[row_cnt][col_cnt]!= Infinity ) {
+	  for ( row_cnt = 0 ; row_cnt < row_max; row_cnt++) {
+	    for ( col_cnt = 0; col_cnt < col_max; col_cnt++) {
+		value = this[row_cnt][col_cnt];
+	      if( ! isNaN (value) && value!= Infinity) {
 		if( max == null)
-		  max = this[row_cnt][col_cnt];
-		else
-		  max = Math.max(max, this[row_cnt][col_cnt]);
+		  max = value;
+		else if(max < value)
+		  max = value;
 	      }
 	    }
 	  }
 	  return max;
 	}
+
 });
 
 
@@ -234,8 +274,8 @@ jStat.extend({
 	// transpose a matrix or array
 	transpose : function( arr ) {
 		arr = isArray( arr[0] ) ? arr : [ arr ];
-		var rows = arr[length],
-			cols = arr[0][length],
+		var rows = arr.length,
+			cols = arr[0].length,
 			obj = [],
 			i = 0, j;
 		for ( ; i < cols; i++ ) {
@@ -249,19 +289,18 @@ jStat.extend({
 
 	// map a function to a matrix or vector
 	map : function( arr, func, toAlter ) {
-		arr = isArray( arr[0] ) ? arr : [ arr ];
+		if ( !isArray( arr[0] )) arr = [ arr ];
 		var row = 0,
-			nrow = arr[length],
-			ncol = arr[0][length],
+			nrow = arr.length,
+			ncol = arr[0].length,
 			res = toAlter ? arr : [],
 			col;
 		for ( ; row < nrow; row++ ) {
-			res[row] = res[row] || [];
-			for ( col = 0; col < ncol; col++ ) {
+			if ( !res[row] ) res[row] = [];
+			for ( col = 0; col < ncol; col++ )
 				res[row][col] = func( arr[row][col], row, col );
-			}
 		}
-		return res;
+		return res.length === 1 ? res[0] : res;
 	},
 
 	// destructively alter an array
@@ -271,7 +310,7 @@ jStat.extend({
 
 	// generate a rows x cols matrix according to the supplied function
 	create: function ( rows, cols, func ) {
-		var i, j, res = [];
+		var res = [], i, j;
 		for( i = 0; i < rows; i++ ) {
 			res[i]  = [];
 			for( j = 0; j < cols; j++ ) {
@@ -304,34 +343,37 @@ jStat.extend({
 	// generate sequence
 	seq : function( min, max, length, func ) {
 		var arr = [],
-			hival = 1e15,
-			step = ( max * hival - min * hival ) / (( length - 1 ) * hival );
-		// TODO: replace toFixed value to user configurable parameter
-		for ( ; min <= max; min = ( min * hival + step * hival ) / hival ) arr.push(( func ? func( min ) : min ));
+			hival = calcRdx( min, max ),
+			step = ( max * hival - min * hival ) / (( length - 1 ) * hival ),
+			current = min,
+			cnt = 0;
+		for ( ; current <= max; cnt++, current = ( min * hival + step * hival * cnt ) / hival ) {
+			arr.push(( func ? func( current ) : current ));
+		}
 		return arr;
 	},
 
 	// add a vector or scalar to the vector
 	add : function( arr, arg ) {
-		return isNaN( arg )
-			? jStat.map( arr, function( value, row, col ) { return value + arg[row][col]; })
-		: jStat.map(arr, function ( value ) { return value + arg; });
+		return isNaN( arg ) ?
+			jStat.map( arr, function( value, row, col ) { return value + arg[row][col]; })
+		: jStat.map( arr, function ( value ) { return value + arg; });
 	},
 
 	// TODO: Implement matrix division
 	// matrix division
 	divide : function( arr, arg ) {
-		return isNaN( arg )
-			? false
+		return isNaN( arg ) ?
+			false
 		: jStat.map(arr, function ( value ) { return value / arg; });
 	},
 
 	// matrix multiplication
 	multiply : function( arr, arg ) {
 		var row, col, nrescols, sum,
-			nrow = arr[length],
-			ncol = arr[0][length],
-			res = jStat.zeros( nrow, nrescols = ( isNaN( arg )) ? arg[0][length] : ncol ),
+			nrow = arr.length,
+			ncol = arr[0].length,
+			res = jStat.zeros( nrow, nrescols = ( isNaN( arg )) ? arg[0].length : ncol ),
 			rescols = 0;
 		if( isNaN( arg )) {
 			for( ; rescols < nrescols; rescols++ ) {
@@ -350,8 +392,8 @@ jStat.extend({
 
 	// subtract a vector or scalar from the vector
 	subtract : function( arr, arg ) {
-		return isNaN( arg )
-			? jStat.map( arr, function( value, row, col ) { return value - arg[row][col]; })
+		return isNaN( arg ) ?
+			jStat.map( arr, function( value, row, col ) { return value - arg[row][col]; })
 		: jStat.map( arr, function( value ) { return value - arg; });
 	},
 
@@ -361,12 +403,12 @@ jStat.extend({
 		arg = isArray( arg[0] ) ? arg : [ arg ];
 
 		// convert column to row vector
-		var left = ( arr[0][length] === 1 && arr[length] !== 1 ) ? jStat.transpose( arr ) : arr,
-			right = ( arg[0][length] === 1 && arg[length] !== 1 ) ? jStat.transpose( arg ) : arg,
+		var left = ( arr[0].length === 1 && arr.length !== 1 ) ? jStat.transpose( arr ) : arr,
+			right = ( arg[0].length === 1 && arg.length !== 1 ) ? jStat.transpose( arg ) : arg,
 			res = [],
 			row = 0,
-			nrow = left[length],
-			ncol = left[0][length],
+			nrow = left.length,
+			ncol = left[0].length,
 			sum, col;
 		for( ; row < nrow; row++ ) {
 			res[row] = [];
@@ -376,7 +418,7 @@ jStat.extend({
 			}
 			res[row] = sum;
 		}
-		return ( res[length] === 1 ) ? res[0] : res;
+		return ( res.length === 1 ) ? res[0] : res;
 	},
 
 	// raise every element by a scalar or vector
@@ -397,12 +439,12 @@ jStat.extend({
 	// BUG: Does not work for matrices
 	// computes the norm of the vector
 	norm : function( arr ) {
-		arr = isArray( arr[0] ) ? arr : [ arr ];
-		if( arr[length] > 1 && arr[0][length] > 1 ) {
+		arr = isArray( arr[0] ) ? arr : [arr];
+		if( arr.length > 1 && arr[0].length > 1 ) {
 			// matrix norm
 		} else {
 			// vector norm
-			return Math.sqrt( jStat.dot( arr, arr ) );
+			return Math.sqrt( jStat.dot( arr, arr ));
 		}
 	},
 
@@ -416,8 +458,8 @@ jStat.extend({
 	symmetric : function( arr ) {
 		var issymmetric = true,
 			row = 0,
-			size = arr[length], col;
-		if( arr[length] !== arr[0][length] ) return false;
+			size = arr.length, col;
+		if( arr.length !== arr[0].length ) return false;
 		for ( ; row < size; row++ ) {
 			for ( col = 0; col < size; col++ ) {
 				if ( arr[col][row] !== arr[row][col] ) return false;
@@ -431,10 +473,8 @@ jStat.extend({
 	// sum of an array
 	sum : function( arr ) {
 		var sum = 0,
-			i = arr[length];
-		while( --i >= 0 ) {
-			sum += arr[i];
-		}
+			i = arr.length;
+		while( --i >= 0 ) sum += arr[i];
 		return sum;
 	},
 
@@ -450,12 +490,12 @@ jStat.extend({
 
 	// mean value of an array
 	mean : function( arr ) {
-		return jStat.sum( arr ) / arr[length];
+		return jStat.sum( arr ) / arr.length;
 	},
 
 	// median of an array
 	median : function( arr ) {
-		var arrlen = arr[length],
+		var arrlen = arr.length,
 			_arr = arr.slice().sort( ascNum );
 
 		// check if array is even or odd, then return the appropriate
@@ -464,7 +504,7 @@ jStat.extend({
 
 	// mode of an array
 	mode : function( arr ) {
-		var arrLen = arr[length],
+		var arrLen = arr.length,
 			_arr = arr.slice().sort( ascNum ),
 			count = 1,
 			maxCount = 0,
@@ -493,25 +533,24 @@ jStat.extend({
 				}
 			}
 		}
-
 		return ( numMaxCount === 0 ) ? maxNum : false;
 	},
 
 	// range of an array
 	range : function( arr ) {
 		var _arr = arr.slice().sort( ascNum );
-		return _arr[ _arr[length] - 1 ] - _arr[0];
+		return _arr[ _arr.length - 1 ] - _arr[0];
 	},
 
 	// variance of an array
 	variance : function( arr ) {
 		var mean = jStat.mean( arr ),
 			stSum = 0,
-			i = arr[length] - 1;
+			i = arr.length - 1;
 		for( ; i >= 0; i-- ) {
 			stSum += Math.pow(( arr[i] - mean ), 2 );
 		}
-		return stSum / ( arr[length] - 1 );
+		return stSum / ( arr.length - 1 );
 	},
 
 	// standard deviation of an array
@@ -523,29 +562,33 @@ jStat.extend({
 	meandev : function( arr ) {
 		var devSum = 0,
 			mean = jStat.mean( arr ),
-			i = arr[length] - 1;
+			i = arr.length - 1;
 		for ( ; i >= 0; i-- ) {
-			devSum += Math.abs( arr[ i ] - mean );
+			devSum += Math.abs( arr[i] - mean );
 		}
-		return devSum / arr[length];
+		return devSum / arr.length;
 	},
 
 	// median deviation (median absolute deviation) of an array
 	meddev : function( arr ) {
 		var devSum = 0,
 			median = jStat.median( arr ),
-			i = arr[length] - 1;
+			i = arr.length - 1;
 		for ( ; i >= 0; i-- ) {
-			devSum += Math.abs( arr[ i ] - median );
+			devSum += Math.abs( arr[i] - median );
 		}
-		return devSum / arr[length];
+		return devSum / arr.length;
 	},
 
 	// quartiles of an array
 	quartiles : function( arr ) {
-		var arrlen = arr[length],
+		var arrlen = arr.length,
 			_arr = arr.slice().sort( ascNum );
-		return [ _arr[ Math.round( ( arrlen ) / 4 ) - 1 ], _arr[ Math.round( ( arrlen ) / 2 ) - 1 ], _arr[ Math.round( ( arrlen ) * 3 / 4 ) - 1 ] ];
+		return [
+			_arr[ Math.round(( arrlen ) / 4 ) - 1 ],
+			_arr[ Math.round(( arrlen ) / 2 ) - 1 ],
+			_arr[ Math.round(( arrlen ) * 3 / 4 ) - 1 ]
+		];
 	},
 
 	// covariance of two arrays
@@ -553,10 +596,10 @@ jStat.extend({
 		var u = jStat.mean( arr1 ),
 			v = jStat.mean( arr2 ),
 			sq_dev = [],
-			arr1Len = arr1[length],
+			arr1Len = arr1.length,
 			i = 0;
 		for ( ; i < arr1Len; i++ ) {
-			sq_dev[ i ] = ( arr1[ i ] - u ) * ( arr2[ i ] - v );
+			sq_dev[i] = ( arr1[i] - u ) * ( arr2[i] - v );
 		}
 		return jStat.sum( sq_dev ) / arr1Len;
 	},
@@ -566,39 +609,28 @@ jStat.extend({
 		return jStat.covariance( arr1, arr2 ) / jStat.stdev( arr1 ) / jStat.stdev( arr2 );
 	},
 
+	// Calculate the square of the value being passed
 	sq: function(value) {
 		return value*value;
 	},
 
+	// Multiply a vector with a scalar and returns a vector ( not a matrix)
 	vec_mult: function(arr, factor) {
-		var res = [];
-		for(var i = 0; i< arr.length; i++) {
+		var i,res = [];
+
+		for(i = 0; i< arr.length; i++) {
 			res[i] = arr[i]*factor;
 		}
 		return res;
 	},
 	
+	// Subtracts two vectors
 	vec_sub: function(arr1, arr2) {
-		var res = [];
-		for(var i = 0; i< arr1.length ; i++) {
+		var i, res = [];
+		for(i = 0; i< arr1.length ; i++) {
 			res[i] = arr1[i] - arr2[i];
 		}
 		return res;
-	},
-
-	vec_transpose: function(arr) {
-	
-	m = arr.length;
-	n = arr[0].length;
-	T=jStat.zeros(n,m);
-	
-	for(var i=0;i<m;i++) {
-		for(var j=0;j<n;j++) {
-			T[j][i] = arr[i][j];
-		}
-	}
-	
-	return T;	
 	}
 });
 
