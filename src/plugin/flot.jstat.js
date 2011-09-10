df8fd09b3a4ef3c2888727b1c7132d0dbba63bc8
Florian Pons <florian.pons@gmail.com>
2011-09-10 02:15:07 +0200

    improve plotting function.

--- src/plugin/flot.jstat.js
+++ src/plugin/flot.jstat.js
@@ -14,20 +14,21 @@ var defaults = {
 
 jStat.flot = function( selector, funcs, opts ) {
 	opts = jQuery.extend( true, {}, defaults, opts );
-
 	var data = [],
 		i = 0;
-
-	if ( jQuery.isFunction( funcs[0] )) {
-		for ( ; i < funcs.length; i++ )
-			data.push( jStat.seq( opts.start, opts.stop, opts.step, function( x ) { return [ x, funcs[i]( x )]; }));
-	} else {
-		for ( ; i < funcs.length; i++ ) {
-			funcs[i].data = jStat.seq( opts.start, opts.stop, opts.step, function( x ) { return [ x, funcs[i].data( x )]; });
+	if (opts.rawData == undefined | !opts.rawData) {
+		if ( jQuery.isFunction( funcs[0] )) {
+			for ( ; i < funcs.length; i++ )
+				data.push( jStat.seq( opts.start, opts.stop, opts.step, function( x ) { return [ x, funcs[i]( x )]; }));
+		} else {
+			for ( ; i < funcs.length; i++ ) {
+				funcs[i].data = jStat.seq( opts.start, opts.stop, opts.step, function( x ) { return [ x, funcs[i].data( x )]; });
+			}
+			data = funcs;
 		}
+	} else {
 		data = funcs;
-	}
-
+	};
 	return jQuery.plot( selector, data, opts.flotopts);
 };
 
