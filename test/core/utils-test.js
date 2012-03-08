4ca723b3cf0fa2383ce3f5ecfbe614e932bae38c
Trevor Norris <trev.norris@gmail.com>
2012-03-08 15:58:15 -0800

    begin revamp of unit and speed tests to run command line

---
+++ test/core/utils-test.js
@@ -0,0 +1,45 @@
+var vows = require( 'vows' ),
+	assert = require('assert' )
+	suite = vows.describe( 'jStat.utils' );
+
+require( '../env.js' );
+
+suite.addBatch({
+	'jStat' : {
+		'topic' : function() {
+			return jStat.utils;
+		},
+		'calcRdx' : function( utils ) {
+			assert.equal( utils.calcRdx( 1e5, 1e10 ), 1e7 );
+		},
+		'isArray' : function( utils ) {
+			assert.isFalse( utils.isArray( true ));
+			assert.isTrue( utils.isArray( [] ));
+			assert.isFalse( utils.isArray( {} ));
+			assert.isFalse( utils.isArray( '123' ));
+			assert.isFalse( utils.isArray( /reg/ ));
+			assert.isFalse( utils.isArray( 1234 ));
+			assert.isFalse( utils.isArray( function() {} ));
+		},
+		'isFunction' : function( utils ) {
+			assert.isFalse( utils.isFunction( true ));
+			assert.isFalse( utils.isFunction( [] ));
+			assert.isFalse( utils.isFunction( {} ));
+			assert.isFalse( utils.isFunction( '123' ));
+			assert.isFalse( utils.isFunction( /reg/ ));
+			assert.isFalse( utils.isFunction( 1234 ));
+			assert.isTrue( utils.isFunction( function() {} ));
+		},
+		'isNumber' : function( utils ) {
+			assert.isFalse( utils.isNumber( true ));
+			assert.isFalse( utils.isNumber( [] ));
+			assert.isFalse( utils.isNumber( {} ));
+			assert.isFalse( utils.isNumber( '123' ));
+			assert.isFalse( utils.isNumber( /reg/ ));
+			assert.isTrue( utils.isNumber( 1234 ));
+			assert.isFalse( utils.isNumber( function() {} ));
+		}
+	}
+});
+
+suite.export( module );
