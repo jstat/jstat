8e3b97a10d80e34b16fbd03d39fc71f01f132119
Trevor Norris <trev.norris@gmail.com>
2012-06-06 13:34:35 -0700

    performance improvements. remove accidental globals. logic simplification.

--- src/test.js
+++ src/test.js
@@ -1,170 +1,172 @@
 (function( jStat, Math ) {
 
+var slice = [].slice,
+	isNumber = jStat.utils.isNumber;
+
 // flag==true denotes use of sample standard deviation
 // Z Statistics
 jStat.extend({
+	// 2 different parameter lists:
+	// ( value, mean, sd )
+	// ( value, array, flag )
 	zscore : function() {
-		// 2 different parameter lists:
-		// ( value, mean, sd )
-		// ( value, array, flag )
-		if ( typeof arguments[1] == "number" ) {
-			return ( arguments[0] - arguments[1] ) / arguments[2];
+		var args = slice.call( arguments );
+		if ( isNumber( args[1] )) {
+			return ( args[0] - args[1] ) / args[2];
 		}
-		return ( arguments[0] - jStat.mean( arguments[1] )) / jStat.stdev( arguments[1], arguments[2] );
+		return ( args[0] - jStat.mean( args[1] )) / jStat.stdev( args[1], args[2] );
 	},
 
+	// 3 different paramter lists:
+	// ( value, mean, sd, sides )
+	// ( zscore, sides )
+	// ( value, array, sides, flag )
 	ztest : function() {
-		// 3 different paramter lists:
-		// ( value, mean, sd, sides )
-		// ( zscore, sides )
-		// ( value, array, sides, flag )
-		if ( arguments.length == 4 ) {
-			if( typeof arguments[1] == "number" ) {
-				if ( arguments[3] == 1 ) {
-					return 1 - jStat.normal.cdf( Math.abs( arguments[0] ), arguments[1], arguments[2] );
-				}
-				return 1 - 2 * jStat.normal.cdf( Math.abs( arguments[0] ), arguments[1], arguments[2] );
+		var args = slice.call( arguments );
+		if ( args.length === 4 ) {
+			if( isNumber( args[1] )) {
+				return ( args[3] === 1 ) ?
+					( 1 - jStat.normal.cdf( Math.abs( args[0] ), args[1], args[2] )) :
+				( 1 - 2 * jStat.normal.cdf( Math.abs( args[0] ), args[1], args[2] ));
 			}
-			if ( arguments[2] == 1 ) {
-				return 1 - jStat.normal.cdf( Math.abs( arguments[0] ), jStat.mean( arguments[1] ), jStat.stdev( arguments[1],arguments[3] ));
-			}
-			return 1 - 2 * jStat.normal.cdf( Math.abs( arguments[0] ), jStat.mean( arguments[1] ), jStat.stdev( arguments[1],arguments[3] ));
+			return ( args[2] === 1 ) ?
+				( 1 - jStat.normal.cdf( Math.abs( args[0] ), jStat.mean( args[1] ), jStat.stdev( args[1],args[3] ))) :
+			( 1 - 2 * jStat.normal.cdf( Math.abs( args[0] ), jStat.mean( args[1] ), jStat.stdev( args[1],args[3] )));
 		}
-		if ( arguments[1] == 1 ) {
-			return 1 - jStat.normal.cdf( Math.abs( arguments[0] ), 0, 1 );
-		}
-		return 1 - 2 * jStat.normal.cdf( Math.abs( arguments[0] ), 0, 1 );
+		return ( args[1] === 1 ) ?
+			( 1 - jStat.normal.cdf( Math.abs( args[0] ), 0, 1 )) :
+		( 1 - 2 * jStat.normal.cdf( Math.abs( args[0] ), 0, 1 ));
 	}
 });
 
 jStat.extend( jStat.fn, {
 	zscore : function( value, flag ) {
-		return ( value - this.mean()) / jStat.stdev( this.toArray(), flag );
+		return ( value - this.mean()) / this.stdev( flag );
 	},
 
 	ztest : function( value, sides, flag ) {
 		var zscore = Math.abs( this.zscore( value, flag ));
-		if( sides == 1 ) {
-			return 1 - jStat.normal.cdf( zscore, 0, 1 );
-		}
-		return 2 * ( 1 - jStat.normal.cdf( zscore, 0, 1 ));
+		return ( sides === 1 ) ?
+			( 1 - jStat.normal.cdf( zscore, 0, 1 )) :
+		( 2 * ( 1 - jStat.normal.cdf( zscore, 0, 1 )));
 	}
 });
 
-//T Statistics
+// T Statistics
 jStat.extend({
+	// 2 parameter lists
+	// ( value, mean, sd, n )
+	// ( value, array )
 	tscore : function() {
-		// 2 parameter lists
-		// ( value, mean, sd, n )
-		// ( value, array )
-		if ( arguments.length == 4 ) {
-			return ( arguments[0] - arguments[1] ) / ( arguments[2] / Math.sqrt( arguments[3] ));
-		}
-		return ( arguments[0] - jStat.mean( arguments[1] )) / ( jStat.stdev( arguments[1], true ) / Math.sqrt( arguments[1].length ));
+		var args = slice.call( arguments );
+		return ( args.length === 4 ) ?
+			(( args[0] - args[1] ) / ( args[2] / Math.sqrt( args[3] ))) :
+		(( args[0] - jStat.mean( args[1] )) / ( jStat.stdev( args[1], true ) / Math.sqrt( args[1].length )));
 	},
 
+	// 3 different paramter lists:
+	// ( value, mean, sd, n, sides )
+	// ( tscore, n, sides )
+	// ( value, array, sides )
 	ttest : function() {
-		// 3 different paramter lists:
-		// ( value, mean, sd, n, sides )
-		// ( tscore, n, sides )
-		// ( value, array, sides )
-		if ( arguments.length == 5 ) {
-			var tscore = Math.abs( jStat.tscore( arguments[0], arguments[1], arguments[2], arguments[3] ));
-			if( arguments[4] == 1 ) {
-				return 1 - jStat.studentt.cdf( tscore, arguments[3] - 1 );
-			}
-			return 1 - ( 2 * jStat.studentt.cdf( tscore, arguments[3] - 1 ));
+		var args = slice.call( arguments ),
+			tscore;
+		if ( args.length === 5 ) {
+			tscore = Math.abs( jStat.tscore( args[0], args[1], args[2], args[3] ));
+			return ( args[4] === 1 ) ?
+				( 1 - jStat.studentt.cdf( tscore, args[3] - 1 )) :
+			( 1 - ( 2 * jStat.studentt.cdf( tscore, args[3] - 1 )));
 		}
-		if ( typeof arguments[1] == "number" ) {
-			tscore = Math.abs( arguments[0] )
-			if ( arguments[2] == 1 ) {
-				return 1 - jStat.studentt.cdf( tscore, arguments[1] - 1 );
-			}
-			return 2 * ( 1 - jStat.studentt.cdf( tscore, arguments[1] - 1 ));
+		if ( isNumber( args[1] )) {
+			tscore = Math.abs( args[0] )
+			return ( args[2] == 1 ) ?
+				( 1 - jStat.studentt.cdf( tscore, args[1] - 1 )) :
+			( 2 * ( 1 - jStat.studentt.cdf( tscore, args[1] - 1 )));
 		}
-		var tscore = Math.abs( jStat.tscore( arguments[0], arguments[1] ))
-		if ( arguments[2] == 1 ) {
-			return 1 - jStat.studentt.cdf( tscore, arguments[1].length - 1 );
-		}
-		return 2 * ( 1 - jStat.studentt.cdf( tscore, arguments[1].length - 1 ));
+		tscore = Math.abs( jStat.tscore( args[0], args[1] ))
+		return ( args[2] == 1 ) ?
+			( 1 - jStat.studentt.cdf( tscore, args[1].length - 1 )) :
+		( 2 * ( 1 - jStat.studentt.cdf( tscore, args[1].length - 1 )));
 	}
 });
 
 jStat.extend( jStat.fn, {
 	tscore : function( value ) {
-		return ( value -this.mean()) / ( jStat.stdev( this.toArray(), true ) / Math.sqrt( this.cols()));
+		return ( value - this.mean()) / ( this.stdev( true ) / Math.sqrt( this.cols()));
 	},
 
 	ttest : function( value, sides ) {
-		if ( sides == 1 ) {
-			return 1 - jStat.studentt.cdf( Math.abs( this.tscore(3)), this.cols() - 1 );
-		}
-		return 2 * ( 1 - jStat.studentt.cdf( Math.abs( this.tscore(3)), this.cols() - 1 ));
+		return ( sides === 1 ) ?
+			( 1 - jStat.studentt.cdf( Math.abs( this.tscore(3)), this.cols() - 1 )) :
+		( 2 * ( 1 - jStat.studentt.cdf( Math.abs( this.tscore(3)), this.cols() - 1 )));
 	}
 });
 
-//F Statistics
+// F Statistics
 jStat.extend({
+	// Paramter list is as follows:
+	// ( array1, array2, array3, ... )
+	// or it is an array of arrays
+	// array of arrays conversion
 	anovafscore : function() {
-		// Paramter list is as follows:
-		// ( array1, array2, array3, ... )
-		// or it is an array of arrays
-		// array of arrays conversion
-		if ( arguments.length == 1 ) {
-			args = new Array( arguments[0].length );
-			for( var i = 0; i < arguments[0].length; i++ ) {
-				args[i] = arguments[0][i];
+		var args = slice.call( arguments ),
+			expVar, sample, sampMean, sampSampMean, tmpargs, unexpVar, i, j;
+		if ( args.length === 1 ) {
+			tmpargs = new Array( args[0].length );
+			for ( i = 0; i < args[0].length; i++ ) {
+				tmpargs[i] = args[0][i];
 			}
-			arguments = args;
+			args = tmpargs;
 		}
 		// 2 sample case
-		if ( arguments.length == 2 ) {
-			return jStat.variance( arguments[0] ) / jStat.variance( arguments[1] );
+		if ( args.length === 2 ) {
+			return jStat.variance( args[0] ) / jStat.variance( args[1] );
 		}
-		//Builds sample array
-		var sample = new Array();
-		for ( var i=0; i < arguments.length; i++ ) {
-			sample = sample.concat(arguments[i]);
+		// Builds sample array
+		sample = new Array();
+		for ( i = 0; i < args.length; i++ ) {
+			sample = sample.concat( args[i] );
 		}	
-		var sampMean=jStat.mean( sample );
-		//Computes the explained variance
-		var expVar = 0;
-		for ( var i = 0; i < arguments.length; i++ ) {
-			expVar = expVar + arguments[i].length * Math.pow( jStat.mean( arguments[i] ) - sampMean, 2 );
+		sampMean = jStat.mean( sample );
+		// Computes the explained variance
+		expVar = 0;
+		for ( i = 0; i < args.length; i++ ) {
+			expVar = expVar + args[i].length * Math.pow( jStat.mean( args[i] ) - sampMean, 2 );
 		}
-		expVar = expVar / ( arguments.length - 1 );
-		//Computes unexplained variance
-		var unexpVar = 0;
-		for ( var i = 0; i < arguments.length; i++ ) {
-			var sampSampMean = jStat.mean( arguments[i] );
-			for ( var j = 0; j < arguments[i].length; j++ ) {
-				unexpVar = unexpVar + Math.pow( arguments[i][j] - sampSampMean, 2 );
+		expVar /= ( args.length - 1 );
+		// Computes unexplained variance
+		unexpVar = 0;
+		for ( i = 0; i < args.length; i++ ) {
+			sampSampMean = jStat.mean( args[i] );
+			for ( j = 0; j < args[i].length; j++ ) {
+				unexpVar += Math.pow( args[i][j] - sampSampMean, 2 );
 			}
 		}
-		unexpVar = unexpVar / ( sample.length - arguments.length );
+		unexpVar /= ( sample.length - args.length );
 		return expVar / unexpVar;
 	},
 
+	// 2 different paramter setups
+	// ( array1, array2, array3, ... )
+	// ( anovafscore, df1, df2 )
 	anovaftest : function() {
-		// 2 different paramter setups
-		// ( array1, array2, array3, ... )
-		// ( anovafscore, df1, df2 )
-		if ( typeof arguments[0] == "number" ) {
-			return 1 - jStat.centralF.cdf( arguments[0], arguments[1], arguments[2] );
+		var args = slice.call( arguments ),
+			df1, df2, n, i;
+		if ( isNumber( args[0] )) {
+			return 1 - jStat.centralF.cdf( args[0], args[1], args[2] );
 		}
-		anovafscore = jStat.anovafscore( arguments );
-		df1 = arguments.length - 1;
-		var n = 0
-		for ( var i = 0; i < arguments.length; i++ ) {
-			n = n + arguments[i].length;
+		anovafscore = jStat.anovafscore( args );
+		df1 = args.length - 1;
+		n = 0;
+		for ( i = 0; i < args.length; i++ ) {
+			n = n + args[i].length;
 		}
 		df2 = n - df1 - 1;
 		return 1 - jStat.centralF.cdf( anovafscore, df1, df2 );
 	},
 
-	ftest : function() {
-		// ( fscore, df1, df2 )
-		return 1 - jStat.centralF.cdf( arguments[0], arguments[1], arguments[2] );
+	ftest : function( fscore, df1, df2 ) {
+		return 1 - jStat.centralF.cdf( fscore, df1, df2 );
 	}
 });
 
@@ -174,51 +176,52 @@ jStat.extend( jStat.fn, {
 	},
 
 	anovaftest: function() {
-		var n = 0;
-		for ( var i = 0; i < this.length; i++ ) {
+		var n = 0,
+			i;
+		for ( i = 0; i < this.length; i++ ) {
 			n = n + this[i].length;
 		}
 		return jStat.ftest( this.anovafscore(), this.length - 1, n - this.length );
 	}
 });
 
-//Error Bounds
+// Error Bounds
 jStat.extend({
+	// 2 different parameter setups
+	// ( value, alpha, sd, n )
+	// ( value, alpha, array )
 	normalci : function() {
-		// 2 different parameter setups
-		// ( value, alpha, sd, n )
-		// ( value, alpha, array )
-		var ans = new Array(2);
-		if ( arguments.length == 4 ) {
-			change = Math.abs( jStat.normal.inv( arguments[1] / 2, 0, 1 ) * arguments[2] / Math.sqrt( arguments[3] ));
-			ans[0] = arguments[0] - change;
-			ans[1] = arguments[0] + change;
-			return ans;
+		var args = slice.call( arguments ),
+			ans = new Array(2),
+			change;
+		if ( args.length === 4 ) {
+			change = Math.abs( jStat.normal.inv( args[1] / 2, 0, 1 ) * args[2] / Math.sqrt( args[3] ));
+		} else {
+			change = Math.abs( jStat.normal.inv( args[1] / 2, 0, 1 ) * jStat.stdev( args[2] ) / Math.sqrt( args[2].length ));
 		}
-		change = Math.abs( jStat.normal.inv( arguments[1] / 2, 0, 1 ) * jStat.stdev( arguments[2] ) / Math.sqrt( arguments[2].length ));
-		ans[0] = arguments[0] - change;
-		ans[1] = arguments[0] + change;
-		return ans
-	},
-
-	tci : function() {
-		// 2 different parameter setups
-		// ( value, alpha, sd, n )
-		// ( value, alpha, array )
-		var ans = new Array(2);
-		if ( arguments.length == 4 ) {
-			change = Math.abs( jStat.studentt.inv( arguments[1] / 2, arguments[3] - 1 ) * arguments[2] / Math.sqrt( arguments[3] ));
-			ans[0] = arguments[0] - change;
-			ans[1] = arguments[0] + change;
-			return ans;
-		}
-		change = Math.abs( jStat.studentt.inv( arguments[1] / 2, arguments[2].length ) * jStat.stdev( arguments[2], true ) / Math.sqrt( arguments[2].length ));
-		ans[0] = arguments[0] - change;
-		ans[1] = arguments[0] + change;
+		ans[0] = args[0] - change;
+		ans[1] = args[0] + change;
 		return ans;
 	},
 
-	significant: function( pvalue, alpha ) {
+	// 2 different parameter setups
+	// ( value, alpha, sd, n )
+	// ( value, alpha, array )
+	tci : function() {
+		var args = slice.call( arguments ),
+			ans = new Array(2),
+			change;
+		if ( args.length === 4 ) {
+			change = Math.abs( jStat.studentt.inv( args[1] / 2, args[3] - 1 ) * args[2] / Math.sqrt( args[3] ));
+		} else {
+			change = Math.abs( jStat.studentt.inv( args[1] / 2, args[2].length ) * jStat.stdev( args[2], true ) / Math.sqrt( args[2].length ));
+		}
+		ans[0] = args[0] - change;
+		ans[1] = args[0] + change;
+		return ans;
+	},
+
+	significant : function( pvalue, alpha ) {
 		return pvalue < alpha;
 	}
 });
