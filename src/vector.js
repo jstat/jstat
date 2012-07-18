d08d410d00ea8bb175aa212b484736fb904e4c93
Tushar Gupta <tushargupta51@gmail.com>
2012-07-18 10:33:57 +0530

    instance method for cumsum cleaned up

--- src/vector.js
+++ src/vector.js
@@ -257,34 +257,33 @@ jStat.extend({
 
 // extend jStat.fn with method for calculating cumulative sums, as it does not run again in case of true
 (function( funcs ) {
-	for ( var i = 0; i < funcs.length; i++ ) (function( passfunc ) {
-		// if a matrix is passed, automatically assume operation should be done on the columns
-		jStat.fn[ passfunc ] = function( fullbool, func ) {
-			var arr = [],
-				i = 0,
-				tmpthis = this;
-			// assignment reassignation depending on how parameters were passed in
-			if ( isFunction( fullbool )) {
-				func = fullbool;
-				fullbool = false;
-			}
-			// check if a callback was passed with the function
-			if ( func ) {
-				setTimeout( function() {
-					func.call( tmpthis, jStat.fn[ passfunc ].call( tmpthis, fullbool ));
-				}, 15 );
-				return this;
-			}
-			// check if matrix and run calculations
-			if ( this.length > 1 ) {
-				tmpthis = fullbool === true ? this : this.transpose();
-				for ( ; i < tmpthis.length; i++ )
-					arr[i] = jStat[ passfunc ]( tmpthis[i] );
-				return arr;
-			}
-		};
-	})( funcs[i] );
-})( 'cumsum'.split( ' ' ));
+		
+	// if a matrix is passed, automatically assume operation should be done on the columns
+	jStat.fn.cumsum = function( fullbool, func ) {
+		var arr = [],
+			i = 0,
+			tmpthis = this;
+		// assignment reassignation depending on how parameters were passed in
+		if ( isFunction( fullbool )) {
+			func = fullbool;
+			fullbool = false;
+		}
+		// check if a callback was passed with the function
+		if ( func ) {
+			setTimeout( function() {
+				func.call( tmpthis, jStat.fn.cumsum.call( tmpthis, fullbool ));
+			}, 15 );
+			return this;
+		}
+		// check if matrix and run calculations
+		if ( this.length > 1 ) {
+			tmpthis = fullbool === true ? this : this.transpose();
+			for ( ; i < tmpthis.length; i++ )
+				arr[i] = jStat.cumsum( tmpthis[i] );
+			return arr;
+		}
+	};
+})('cumsum');
 
 
 }( this.jStat, Math ));
