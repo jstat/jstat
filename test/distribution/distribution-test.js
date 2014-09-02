5cc8c569d159061b1bc1e5815f9c049007b1e76d
James Gibson <james@innovategsb.com>
2014-09-02 13:11:49 -0400

    Return correct values for jStat.studentt.inv
    Added tests for the studentt.pdf, .cdf, and .inv methods. Fixed an issue
    with the .inv method (always returned positive values).
    
    Fixes: #88
    Reviewed-by: Trevor Norris <trev.norris@gmail.com>

--- test/distribution/distribution-test.js
+++ test/distribution/distribution-test.js
@@ -66,6 +66,35 @@ suite.addBatch({
 });
 
 suite.addBatch({
+  'studentt pdf': {
+    'topic' : function() {
+      return jStat;
+    },
+    //Checked against R dt(x,df)
+    'check pdf calculation' : function(jStat) {
+      var tol = 0.0000001;
+      var x = 0.5;
+      var df = 40;
+      assert.epsilon(tol, jStat.studentt.pdf(x, df), 0.3489195);
+    },
+    //Checked against R's pt(x, df)
+    'check cdf calculation' : function(jStat) {
+      var tol = 0.0000001;
+      var x = 0.5;
+      var df = 40;
+      assert.epsilon(tol, jStat.studentt.cdf(x, df), 0.6900926);
+    },
+    //Checked against R's qt(x, df)
+    'check inv calculation' : function(jStat) {
+      var tol = 0.0000001;
+      assert.epsilon(tol, jStat.studentt.inv(0.5, 40), 0);
+      assert.epsilon(tol, jStat.studentt.inv(0.7, 20), 0.5328628);
+      assert.epsilon(tol, jStat.studentt.inv(0.2, 10), -0.8790578);
+    }
+  }
+});
+
+suite.addBatch({
   'negbin pdf': {
     'topic' : function() {
       return jStat;
