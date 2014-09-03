e5d6783677db55549af2245e2c94daa664c7c7cf
James Gibson <james@innovategsb.com>
2014-09-03 15:24:42 -0400

    Add Tests for Binomial Distribution
    Added automated tests for the binomial distribution for future
    refactoring.
    
    Signed-off-by: Trevor Norris <trev.norris@gmail.com>

--- test/distribution/distribution-test.js
+++ test/distribution/distribution-test.js
@@ -66,6 +66,26 @@ suite.addBatch({
 });
 
 suite.addBatch({
+  'binomial pdf': {
+    'topic' : function() {
+      return jStat;
+    },
+    //checked against R's dbinom(k, n, p)
+    'check pdf calculation' : function(jStat) {
+      var tol = 0.0000001;
+      assert.epsilon(tol, jStat.binomial.pdf(10, 25, 0.5), 0.09741664);
+      assert.epsilon(tol, jStat.binomial.pdf(50, 1000, 0.05), 0.05778798);
+    },
+    //Checked against r's pbinom(k, n, p)
+    'check cdf calculation' : function(jStat) {
+      var tol = 0.0000001;
+      assert.epsilon(tol, jStat.binomial.cdf(10, 25, 0.5), 0.2121781);
+      assert.epsilon(tol, jStat.binomial.cdf(50, 1000, 0.05), 0.537529);
+    }
+  }
+});
+
+suite.addBatch({
   'studentt pdf': {
     'topic' : function() {
       return jStat;
