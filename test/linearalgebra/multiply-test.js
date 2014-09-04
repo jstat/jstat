1b0f89f62502d9589c2502020d987733f49ebcee
James Gibson <james@innovategsb.com>
2014-09-04 00:00:15 -0400

    Add tests for various linear algebra functions
    Additionally, fixed a small bug that prevented .dot() and .angle() from
    being called on a jStat object, as opposed to as a static function.
    
    There are still untested functions in the linear algebra library, and it
    does not make sense that all of the linear algebra functions can be
    called on a jStat() object but only accept plain matricies as arguments.
    This future change will be made easier with fuller test coverage.
    
    Reviewed-by: Trevor Norris <trev.norris@gmail.com>

---
+++ test/linearalgebra/multiply-test.js
@@ -0,0 +1,24 @@
+var vows = require('vows');
+var assert = require('assert');
+var suite = vows.describe('jStat');
+
+require('../env.js');
+
+suite.addBatch({
+  'linearalgebra': {
+    'topic': function() {
+      return jStat;
+    },
+    'test that multipling by a scalar works': function(jStat) {
+      var A = jStat([[1, 2, 3]]);
+      assert.deepEqual(A.multiply(5), jStat([[5, 10, 15]]));
+    },
+    'test that multiplying by a matrix works': function(jStat) {
+      var A = jStat([[1, 2], [3, 4]]);
+      var B = [[1, 0], [0, 1]];
+      assert.deepEqual(A.multiply(B), jStat([[1, 2], [3, 4]]));
+    }
+  }
+});
+
+suite.export(module);
