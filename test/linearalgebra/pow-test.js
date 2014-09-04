0e7be61dd3c6cc18f753ac829f6b3aa86f928d18
James Gibson <james@innovategsb.com>
2014-09-04 00:00:15 -0400

    Added tests for many of the linear algebra functions.
    Additionally, fixed a small bug that prevented .dot() and .angle() from
    being called on a jStat object, as opposed to as a static function.
    
    There are still untested functions in the linear algebra library,
    and it does not make sense that all of the linear algebra functions
    can be called on a jStat() object but only accept plain matricies
    as arguments. This future change will be made easier with fuller test
    coverage.

---
+++ test/linearalgebra/pow-test.js
@@ -0,0 +1,19 @@
+var vows = require('vows'),
+	assert = require('assert')
+	suite = vows.describe('jStat');
+
+require('../env.js');
+
+suite.addBatch({
+	'linearalgebra' : {
+		'topic' : function() {
+			return jStat;
+		},
+		'pow function works' : function(jStat) {
+      var A = jStat([[1, 2], [3, 4]]);
+      assert.deepEqual(A.pow(2), jStat([[1, 4], [9, 16]]));
+		}
+	}
+});
+
+suite.export(module);
