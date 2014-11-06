3b308969f8ac0904aac7ab8c2564d4111aec31d5
James Gibson <james@innovategsb.com>
2014-11-06 18:19:10 -0500

    Fix #109, define uniform.inv & test
    Also added tests for previously untested uniform.pdf and uniform.cdf
    functions.

--- src/distribution.js
+++ src/distribution.js
@@ -581,6 +581,10 @@ jStat.extend(jStat.uniform, {
     return 1;
   },
 
+  inv: function(p, a, b) {
+    return a + (p * (b - a));
+  },
+
   mean: function mean(a, b) {
     return 0.5 * (a + b);
   },
