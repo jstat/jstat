86575ce21c3d820160dee5495d826a8b6c57d382
James Gibson <james@innovategsb.com>
2014-12-26 23:46:28 -0500

    Remove unneeded .isNumber calls
    Vows .epislon now checks if the argument is a number, no longer
    necessary.

--- test/distribution/central-f-test.js
+++ test/distribution/central-f-test.js
@@ -14,24 +14,20 @@ suite.addBatch({
       var tol = 0.0000001;
 
       var zeroth = jStat.centralF.pdf(0.2, 1, 3);
-      assert.isNumber(zeroth);
       assert.epsilon(tol, zeroth, 0.722349);
 
       var first = jStat.centralF.pdf(1, 100, 100);
-      assert.isNumber(first);
       assert.epsilon(tol, first, 1.989731);
 
       var second = jStat.centralF.pdf(2.5, 50, 200);
-      assert.isNumber(second);
       assert.epsilon(tol, second, 0.00003610325);
 
       var third = jStat.centralF.pdf(0.8, 2, 10);
-      assert.isNumber(third);
       assert.epsilon(tol, third, 0.4104423);
 
-      var value = jStat.centralF.pdf(0.4, 3, 10);
-      assert.isNumber(value);
-      assert.epsilon(tol, value, 0.6733766);
+      var fourth = jStat.centralF.pdf(0.4, 3, 10);
+      assert.epsilon(tol, fourth, 0.6733766);
+
     }
   },
 });
