0c904e621c39810198d3a1c107f485a8536c2514
James Gibson <james@spacedolphin.io>
2015-06-23 22:06:15 -0400

    Move jstat build into script

---
+++ build/docs.js
@@ -0,0 +1,25 @@
+/*
+	Build the docs
+*/
+
+var fs = require("fs");
+var path = require("path");
+var mkdirp = require('mkdirp');
+
+var file_names = ["./src/core.js", "./src/vector.js", "./src/special.js",
+                  "./src/distribution.js", "./src/linearalgebra.js", "./src/test.js"];
+var files = [];
+
+mkdirp('dist', function(err) {
+  if(err) {
+    console.error(err)
+  }
+});
+
+for(var i = 0; i < file_names.length; i++) {
+  files.push(fs.readFileSync(file_names[i]));
+}
+
+fs.writeFileSync('./dist/jstat.js', Buffer.concat(files));
+
+
