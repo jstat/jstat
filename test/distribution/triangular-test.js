85ab6a70eebef97b2b834a41733502d063ebc5b6
akrawitz <akrawitz@uvic.ca>
2015-07-13 13:20:06 -0700

    Update tests for triangular.pdf
    Updating and adding tests for triangular.pdf(). One of the previous
    tests was:
    assert.epsilon(tol, jStat.triangular.pdf(1.0, 1.0, 2.0, 1.0), 1.0);
    This test was failing, however the test is incorrect. A comment in the
    test code claimed that this test was based on R's dtriangle(x, a, b, c)
    from package 'triangle', however this is not consistent with the current
    return value for the R code:
    > dtriangle(1, 1, 2, 1)
    [1] NaN
    Warning message:
    In dTest(c(x, a, b, c)) :
    values required to be  a <= c <= b (at least one strict inequality)
    
    To further confuse matters, dtriangle() clearly has a bug in it, given
    that NaN is not the correct value for this case, and the provided
    warning is not even consistent with the passed parameters!
    
    So I am now using R's dtriang() from package mc2d as my reference, as it
    appears to be a better implementation of the triangular distribution.
    
    I have added the reference R code into the comment in the test file,
    since if we are going to refer to R functions for tests, we might as
    well make it easy for someone to confirm.
    
    Furthermore, I have added more tests, in an attempt to get basic
    coverage of the various segments of the triangular distribution for each
    of the basic forms it can take.

--- test/distribution/triangular-test.js
+++ test/distribution/triangular-test.js
@@ -9,12 +9,59 @@ suite.addBatch({
     'topic': function() {
       return jStat;
     },
-    // checked against R's dtriangle(x, a, b, c) from package 'triangle'
-    'check pdf calculation': function(jStat) {
+    // checked against R's dtriang(x, min, mode, max, log=FALSE) from package 'mc2d':
+    //   install.packages("mc2d")
+    //   library("mc2d")
+    //   dtriang(c(0.5, 1, 2.5, 5, 6.5, 11, 20), 1, 5, 11)
+    //   dtriang(c(-20, -5, -2.5, 5, 10), -5, -5, 5)
+    //   dtriang(c(-10, 0, 4, 8, 12), 0, 8, 8)
+    //   dtriang(c(0, 7, 12), 5, 4, 10)
+    //   dtriang(c(17, 30, 88), 23, 50, 47)
+    //   dtriang(c(-17, -10, 0), -10, -10, -10)
+    
+    'check pdf calculation, when a < c < b': function(jStat) {
       var tol = 0.0000001;
-      assert.epsilon(tol, jStat.triangular.pdf(5.5, 1, 10, 5), 0.2);
-      assert.epsilon(tol, jStat.triangular.pdf(1.0, 1.0, 2.0, 1.0), 1.0);
-      assert.epsilon(tol, jStat.triangular.pdf(3.0, 1.0, 3.0, 3.0), 1.0);
+      assert.epsilon(tol, jStat.triangular.pdf(0.5, 1, 11, 5), 0);      // x < a
+      assert.epsilon(tol, jStat.triangular.pdf(1, 1, 11, 5), 0);        // x = a
+      assert.epsilon(tol, jStat.triangular.pdf(2.5, 1, 11, 5), 0.075);  // a < x < c
+      assert.epsilon(tol, jStat.triangular.pdf(5, 1, 11, 5), 0.2);      // x = c
+      assert.epsilon(tol, jStat.triangular.pdf(6.5, 1, 11, 5), 0.15);   // c < x < b
+      assert.epsilon(tol, jStat.triangular.pdf(11, 1, 11, 5), 0);       // x = b
+      assert.epsilon(tol, jStat.triangular.pdf(20, 1, 11, 5), 0);       // b < x
+    },
+    'check pdf calculation, when a = c < b': function(jStat) {
+      var tol = 0.0000001;
+      assert.epsilon(tol, jStat.triangular.pdf(-20, -5, 5, -5), 0);     // x < a
+      assert.epsilon(tol, jStat.triangular.pdf(-5, -5, 5, -5), 0.2);    // x = a = c
+      assert.epsilon(tol, jStat.triangular.pdf(-2.5, -5, 5, -5), 0.15); // a = c < x < b
+      assert.epsilon(tol, jStat.triangular.pdf(5, -5, 5, -5), 0);       // x = b
+      assert.epsilon(tol, jStat.triangular.pdf(10, -5, 5, -5), 0);      // b < x
+    },
+    'check pdf calculation, when a < c = b': function(jStat) {
+      var tol = 0.0000001;
+      assert.epsilon(tol, jStat.triangular.pdf(-10, 0, 8, 8), 0);       // x < a
+      assert.epsilon(tol, jStat.triangular.pdf(0, 0, 8, 8), 0);         // x = a < c = b
+      assert.epsilon(tol, jStat.triangular.pdf(4, 0, 8, 8), 0.125);     // a = c < x < b
+      assert.epsilon(tol, jStat.triangular.pdf(8, 0, 8, 8), 0.25);      // x = c = b
+      assert.epsilon(tol, jStat.triangular.pdf(12, 0, 8, 8), 0);        // b < x
+    },
+    'check pdf calculation, when c < a': function(jStat) {
+      var tol = 0.0000001;
+      assert.isNaN(jStat.triangular.pdf(0, 5, 10, 4));                  // x < a
+      assert.isNaN(jStat.triangular.pdf(7, 5, 10, 4));                  // a < x < b
+      assert.isNaN(jStat.triangular.pdf(12, 5, 10, 4));                 // b < x
+    },
+    'check pdf calculation, when b < c': function(jStat) {
+      var tol = 0.0000001;
+      assert.isNaN(jStat.triangular.pdf(17, 23, 47, 50));               // x < a
+      assert.isNaN(jStat.triangular.pdf(30, 23, 47, 50));               // a < x < b
+      assert.isNaN(jStat.triangular.pdf(88, 23, 47, 50));               // b < x
+    },
+    'check pdf calculation, when a = b': function(jStat) {
+      var tol = 0.0000001;
+      assert.isNaN(jStat.triangular.pdf(-17, -10, -10, -10));           // x < a
+      assert.isNaN(jStat.triangular.pdf(-10, -10, -10, -10));           // a = x = b
+      assert.isNaN(jStat.triangular.pdf(0, -10, -10, -10));             // b < x
     },
   }
 });
