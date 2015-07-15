1213f1b2ef60639a84090ce838841716ef6f7940
Steve Mardenfeld <smardenfeld@stripe.com>
2015-07-15 16:17:55 -0700

    - add difference of proportions test - differenceOfProprtions throws error if proportion is negative number - add tests for difference of proportions - differenceOfProportion can't have a negative proportion as input - add docs for twoSidedDifferenceofProportions test
    PR-URL: https://github.com/jstat/jstat/pull/158
    Reviewed-By: James Gibson <james.cameron.gibson@gmail.com>

--- src/test.js
+++ src/test.js
@@ -251,4 +251,27 @@ jStat.extend(jStat.fn, {
   }
 });
 
+// internal method for calculating the z-score for a difference of proportions test
+differenceOfProportions: function differenceOfProportions(p1, n1, p2, n2) {
+  if (p1 > 1 || p2 > 1 || p1 <= 0 || p2 <= 0) {
+    throw new Error("Proportions should be greater than 0 and less than 1")
+  }
+  var pooled = (p1 * n1 + p2 * n2) / (n1 + n2);
+  var se = Math.sqrt(pooled * (1 - pooled) * ((1/n1) + (1/n2)));
+  return (p1 - p2) / se;
+}
+
+// Difference of Proportions
+jStat.extend(jStat.fn, {
+  oneSidedDifferenceOfProportions: function oneSidedDifferenceOfProportions(p1, n1, p2, n2) {
+    var z = differenceOfProportions(p1, n1, p2, n2);
+    return jStat.ztest(z, 1);
+  },
+
+  twoSidedDifferenceOfProportions: function twoSidedDifferenceOfProportions(p1, n1, p2, n2) {
+    var z = differenceOfProportions(p1, n1, p2, n2);
+    return jStat.ztest(z, 2);
+  }
+});
+
 }(this.jStat, Math));
