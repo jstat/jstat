6840ac23a85eb1da8cff877c64db2ec7685a866e
akrawitz <akrawitz@uvic.ca>
2015-07-15 12:16:57 -0700

    Fix cdf()sreturn values
    Fix cdf()s to return 0 below support and 1 above support
    
    Fixes for multiple distributions (centralF, chisquare, gamma, invgamma,
    kumaraswamy, lognormal, pareto, triangular) so that cdf()s return proper
    values outside of support.
    
    Also added testing for these cdf()s. If test already existed, just added
    ones outside support, otherwise added multiple tests inside and outside
    support.
    
    PR-URL: https://github.com/jstat/jstat/pull/157
    Reviewed-by: Trevor Norris <trev.norris@gmail.com>

--- test/distribution/pareto-test.js
+++ test/distribution/pareto-test.js
@@ -31,6 +31,32 @@ suite.addBatch({
       assert.equal(jStat.pareto.inv(1, 2, 2), Infinity);
     }
   },
+  'pareto cdf': {
+    'topic': function() {
+      return jStat;
+    },
+    // Checked against R's ppareto(q, scale = 1, shape, lower.tail = TRUE, log.p = FALSE) in package VGAM
+    //   install.packages("VGAM")
+    //   library("VGAM")
+    //   options(digits=10)
+    //   ppareto(c(0, 1, 2), 1, 1)
+    //   ppareto(c(-1, 1, 4), 1, 2)
+    //   ppareto(c(1, 2, 10), 2, 2)
+    'check cdf calculation': function(jStat) {
+      var tol = 0.0000001;
+      assert.epsilon(tol, jStat.pareto.cdf(0, 1, 1), 0);
+      assert.epsilon(tol, jStat.pareto.cdf(1, 1, 1), 0);
+      assert.epsilon(tol, jStat.pareto.cdf(2, 1, 1), 0.5);
+
+      assert.epsilon(tol, jStat.pareto.cdf(-1, 1, 2), 0);
+      assert.epsilon(tol, jStat.pareto.cdf(1, 1, 2), 0);
+      assert.epsilon(tol, jStat.pareto.cdf(4, 1, 2), 0.9375);
+ 
+      assert.epsilon(tol, jStat.pareto.cdf(1, 2, 2), 0);
+      assert.epsilon(tol, jStat.pareto.cdf(2, 2, 2), 0);
+      assert.epsilon(tol, jStat.pareto.cdf(10, 2, 2), 0.96);
+    }
+  },
 });
 
 suite.export(module);
