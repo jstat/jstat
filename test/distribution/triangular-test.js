6db148866130cc6ea286c71d76d43f1ef8bf9fd4
akrawitz <akrawitz@uvic.ca>
2015-09-24 11:46:00 -0600

    Add quantile function for triangular distribution
    The triangular distribution did not have a quantile function (inverse
    cumulative distribution function) defined. The math is based on
    definition 25 on http://www.math.uah.edu/stat/special/Triangle.html, but
    it has been converted that definition to be in terms of the a, b, c
    parameterization instead of the a, w, p parameterization.
    
    PR-URL: https://github.com/jstat/jstat/pull/149
    Reviewed-by: Trevor Norris <trev.norris@gmail.com>

--- test/distribution/triangular-test.js
+++ test/distribution/triangular-test.js
@@ -62,7 +62,7 @@ suite.addBatch({
       assert.isNaN(jStat.triangular.pdf(-17, -10, -10, -10));           // x < a
       assert.isNaN(jStat.triangular.pdf(-10, -10, -10, -10));           // a = x = b
       assert.isNaN(jStat.triangular.pdf(0, -10, -10, -10));             // b < x
-    },
+    }
   },
   'triangular cdf': {
     'topic': function() {
@@ -115,6 +115,59 @@ suite.addBatch({
     'check cdf calculation, when a = b': function(jStat) {
       var tol = 0.0000001;
       assert.isNaN(jStat.triangular.cdf(-10, -10, -10, -10));
+    }
+  },
+  'triangular inv': {
+    'topic': function() {
+      return jStat;
+    },
+    // checked against R's qtriang(p, min=-1, mode=0, max=1, lower.tail=TRUE, log.p=FALSE) from package 'mc2d':
+    //   install.packages("mc2d")
+    //   library("mc2d")
+    //   options(digits=10)
+    //   qtriang(c(0, 0.25, 0.5, 0.75, 1), 1, 5, 11)
+    //   qtriang(c(0, 0.5, 1), -5, -5, 5)
+    //   qtriang(c(0, 0.5, 1), 0, 8, 8)
+    //   qtriang(c(0, 0.5, 1), 5, 4, 10)
+    //   qtriang(c(0, 0.5, 1), 23, 50, 47)
+    //   qtriang(c(0, 0.5, 1), -10, -10, -10) # NOTE: This returns: [1] 1 1 1, but we don't allow a = b = c!
+    'check inv calculation, when a < c < b': function(jStat) {
+      var tol = 0.0000001;
+      assert.epsilon(tol, jStat.triangular.inv(0, 1, 11, 5), 1);
+      assert.epsilon(tol, jStat.triangular.inv(0.25, 1, 11, 5), 4.162277660);
+      assert.epsilon(tol, jStat.triangular.inv(0.5, 1, 11, 5), 5.522774425);
+      assert.epsilon(tol, jStat.triangular.inv(0.75, 1, 11, 5), 7.127016654);
+      assert.epsilon(tol, jStat.triangular.inv(1, 1, 11, 5), 11);
+    },
+    'check inv calculation, when a = c < b': function(jStat) {
+      var tol = 0.0000001;
+      assert.epsilon(tol, jStat.triangular.inv(0, -5, 5, -5), -5);
+      assert.epsilon(tol, jStat.triangular.inv(0.5, -5, 5, -5), -2.071067812);
+      assert.epsilon(tol, jStat.triangular.inv(1, -5, 5, -5), 5);
+    },
+    'check inv calculation, when a < c = b': function(jStat) {
+      var tol = 0.0000001;
+      assert.epsilon(tol, jStat.triangular.inv(0, 0, 8, 8), 0);
+      assert.epsilon(tol, jStat.triangular.inv(0.5, 0, 8, 8), 5.656854249);
+      assert.epsilon(tol, jStat.triangular.inv(1, 0, 8, 8), 8);
+    },
+    'check inv calculation, when c < a': function(jStat) {
+      var tol = 0.0000001;
+      assert.isNaN(jStat.triangular.inv(0, 5, 10, 4));
+      assert.isNaN(jStat.triangular.inv(0.5, 5, 10, 4));
+      assert.isNaN(jStat.triangular.inv(1, 5, 10, 4));
+    },
+    'check inv calculation, when b < c': function(jStat) {
+      var tol = 0.0000001;
+      assert.isNaN(jStat.triangular.inv(0, 23, 47, 50));
+      assert.isNaN(jStat.triangular.inv(0.5, 23, 47, 50));
+      assert.isNaN(jStat.triangular.inv(1, 23, 47, 50));
+    },
+    'check inv calculation, when a = b': function(jStat) {
+      var tol = 0.0000001;
+      assert.isNaN(jStat.triangular.inv(0, -10, -10, -10));
+      assert.isNaN(jStat.triangular.inv(0.5, -10, -10, -10));
+      assert.isNaN(jStat.triangular.inv(1, -10, -10, -10));
     }
   }
 });
