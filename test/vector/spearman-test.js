ccddd66c7e61126c0c44110e0fb1c5f1f2863aea
Pratap Vardhan <pratapgr8@gmail.com>
2015-11-12 23:01:12 +0530

    Add processing of ties to Spearman's Coeffient
    ENH: Spearman rank coeff with ties
    TST: Spearman rank tests with ties
    TST: deviation test cases
    TST: rank test cases
    
    PR-URL: https://github.com/jstat/jstat/issues/172
    Reviewed-By: James Gibson <james.cameron.gibson@gmail.com>

--- test/vector/spearman-test.js
+++ test/vector/spearman-test.js
@@ -12,6 +12,18 @@ suite.addBatch({
     'return basic spearmancoeff': function(jStat) {
       assert.equal(jStat.spearmancoeff([1, 2, 3, 4], [5, 6, 9, 7]),
                    0.8);
+    },
+    'return spearmancoeff with ties': function(jStat) {
+      assert.equal(jStat.spearmancoeff([1, 2, 3, 4], [5, 5, 9, 7]),
+                   0.7378647873726218);
+    },
+    'return spearmancoeff all ties': function(jStat) {
+      assert.equal(isNaN(jStat.spearmancoeff([1, 2, 3, 4], [5, 5, 5, 5])),
+                   true);
+    },
+    'return spearmancoeff unequal arrays': function(jStat) {
+      assert.equal(isNaN(jStat.spearmancoeff([1, 2, 3, 4], [5, 6, 7])),
+                   true);
     }
   }
 });
