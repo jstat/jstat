eeb696a4390c31d85525595a5d5bce1f1e5574ef
Trevor Norris <trev.norris@gmail.com>
2019-06-04 11:59:17 -0600

    Allow binomial.cdf to handle large numbers
    Also add some checks to make sure NaN is returned when it should be.
    
    Fixes: https://github.com/jstat/jstat/issues/228

--- test/distribution/binomial-test.js
+++ test/distribution/binomial-test.js
@@ -20,6 +20,14 @@ suite.addBatch({
       var tol = 0.0000001;
       assert.epsilon(tol, jStat.binomial.cdf(10, 25, 0.5), 0.2121781);
       assert.epsilon(tol, jStat.binomial.cdf(50, 1000, 0.05), 0.537529);
+
+      assert.ok(Number.isNaN(jStat.binomial.cdf(10, 12, -1)));
+      assert.ok(Number.isNaN(jStat.binomial.cdf(10, 12, 2)));
+      assert.equal(jStat.binomial.cdf(12, 10, 0.5), 1);
+      assert.equal(jStat.binomial.cdf(12, -1, 0.5), 1);
+      assert.epsilon(tol,
+                     jStat.binomial.cdf(101073, 101184, 0.9988219676207195),
+                     0.7857313651);
     }
   }
 });
