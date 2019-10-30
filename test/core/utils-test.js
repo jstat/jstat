jstat/jstat

commit a55cee090c6b73ae08d8a530746c4f153efa75e8

Change isNumber implementation
Fixes a minor bug in an extreme edge case.

src/core.js
@@ -25,7 +25,13 @@ function isFunction(arg) {
 
 
 function isNumber(arg) {
-  return typeof arg === 'number' && arg === arg;
+  if (typeof num === 'number') {
+    return num - num === 0;
+  }
+  if (typeof num === 'string' && num.trim() !== '') {
+    return Number.isFinite ? Number.isFinite(+num) : isFinite(+num);
+  }
+  return false;
 }
 
 

dist/jstat.js
@@ -34,7 +34,13 @@ function isFunction(arg) {
 
 
 function isNumber(arg) {
-  return typeof arg === 'number' && arg === arg;
+  if (typeof num === 'number') {
+    return num - num === 0;
+  }
+  if (typeof num === 'string' && num.trim() !== '') {
+    return Number.isFinite ? Number.isFinite(+num) : isFinite(+num);
+  }
+  return false;
 }
 
 

commit bef37864642a70bee49c03f57a0a1da22a04a890

Fix isNumber implementation change
* Didn't rename the argument passed to isNumber, causing false to always
  be returned.
* Be more strict. Strings as numbers are not considered numbers.

Fixes: a55cee0 "Change isNumber implementation"

src/core.js
@@ -24,14 +24,8 @@ function isFunction(arg) {
 }
 
 
-function isNumber(arg) {
-  if (typeof num === 'number') {
-    return num - num === 0;
-  }
-  if (typeof num === 'string' && num.trim() !== '') {
-    return Number.isFinite ? Number.isFinite(+num) : isFinite(+num);
-  }
-  return false;
+function isNumber(num) {
+  return (typeof num === 'number') ? num - num === 0 : false;
 }
 
 

test/core/utils-test.js
@@ -31,13 +31,39 @@ suite.addBatch({
       assert.isTrue(utils.isFunction(function() {}));
     },
     'isNumber': function(utils) {
-      assert.isFalse(utils.isNumber(true));
+      assert.isTrue(utils.isNumber(5e3));
+      assert.isTrue(utils.isNumber(0xff));
+      assert.isTrue(utils.isNumber(-1.1));
+      assert.isTrue(utils.isNumber(0));
+      assert.isTrue(utils.isNumber(1));
+      assert.isTrue(utils.isNumber(1.1));
+      assert.isTrue(utils.isNumber(10));
+      assert.isTrue(utils.isNumber(10.10));
+      assert.isTrue(utils.isNumber(100));
+      assert.isTrue(utils.isNumber(parseInt('012')));
+      assert.isTrue(utils.isNumber(parseFloat('012')));
+
+      assert.isFalse(utils.isNumber('-1.1'));
+      assert.isFalse(utils.isNumber('0'));
+      assert.isFalse(utils.isNumber('012'));
+      assert.isFalse(utils.isNumber('0xff'));
+      assert.isFalse(utils.isNumber('1'));
+      assert.isFalse(utils.isNumber('1.1'));
+      assert.isFalse(utils.isNumber('10'));
+      assert.isFalse(utils.isNumber('10.10'));
+      assert.isFalse(utils.isNumber('100'));
+      assert.isFalse(utils.isNumber('5e3'));
+      assert.isFalse(utils.isNumber(Infinity));
+      assert.isFalse(utils.isNumber(NaN));
+      assert.isFalse(utils.isNumber(null));
+      assert.isFalse(utils.isNumber(undefined));
+      assert.isFalse(utils.isNumber(''));
+      assert.isFalse(utils.isNumber('   '));
+      assert.isFalse(utils.isNumber('foo'));
+      assert.isFalse(utils.isNumber([1]));
       assert.isFalse(utils.isNumber([]));
+      assert.isFalse(utils.isNumber(function () {}));
       assert.isFalse(utils.isNumber({}));
-      assert.isFalse(utils.isNumber('123'));
-      assert.isFalse(utils.isNumber(/reg/));
-      assert.isFalse(utils.isNumber(function() {}));
-      assert.isTrue(utils.isNumber(1234));
     }
   }
 });