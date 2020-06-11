c73cc5f9759c7efe59e4a102245984ef0433e572
Athan Reines <kgryte@gmail.com>
2020-06-11 15:15:13 -0700

    Remove obsolete header file

--- src/_header.js
+++
@@ -1,9 +0,0 @@
-(function (window, factory) {
-    if (typeof exports === 'object') {
-        module.exports = factory();
-    } else if (typeof define === 'function' && define.amd) {
-        define(factory);
-    } else {
-        window.jStat = factory();
-    }
-})(this, function () {
