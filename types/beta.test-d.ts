f839eae5ab2c1ad578b988c204348ebd3eabac17
Brice Leroy <brice@brigalabs.com>
2021-03-03 09:59:44 -0500

    add the beta distribution

---
+++ types/beta.test-d.ts
@@ -0,0 +1,30 @@
+/// <reference path="../index.d.ts" />
+/// <reference path="./beta.d.ts" />
+
+import { jStat } from "jstat";
+import { expectType } from "tsd";
+
+/**
+ * jStat Normal distribution instance
+ */
+const betaD = jStat.beta(0, 1);
+expectType<InstanceType<jStat.BetaDistribution>>(betaD);
+
+expectType<number>(jStat.beta(0, 1).pdf(0.5));
+expectType<number>(jStat.beta(0, 1).cdf(0.5));
+expectType<number>(jStat.beta(0, 1).inv(0.5));
+expectType<number>(jStat.beta(0, 1).mean());
+expectType<number>(jStat.beta(0, 1).median());
+expectType<number>(jStat.beta(0, 1).mode());
+expectType<number>(jStat.beta(0, 1).variance());
+
+/**
+ * jStat beta distribution functions
+ */
+expectType<number>(jStat.beta.pdf(0.5, 0, 1));
+expectType<number>(jStat.beta.cdf(0.5, 0, 1));
+expectType<number>(jStat.beta.inv(0.5, 0, 1));
+expectType<number>(jStat.beta.mean(0, 1));
+expectType<number>(jStat.beta.median(0, 1));
+expectType<number>(jStat.beta.mode(0, 1));
+expectType<number>(jStat.beta.variance(0, 1));
