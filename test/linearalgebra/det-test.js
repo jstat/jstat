ad6f21fdcc4a3bb48563e65e02cafa126647fb12
Brice Leroy <brice@brigalabs.com>
2021-03-03 14:30:55 -0500

    fix issue #270 for det() on large matrix

--- test/linearalgebra/det-test.js
+++ test/linearalgebra/det-test.js
@@ -9,10 +9,26 @@ suite.addBatch({
     'topic': function() {
       return jStat;
     },
+    "test det on 2x2 matrix": function (jStat) {
+      var A = [
+        [4, 6],
+        [3, 8],
+      ];
+      assert.equal(jStat.det(A), 14);
+    },
     'test det works': function(jStat) {
       var A = [[1, 2, 3], [4, 5, -6], [7, -8, 9]];
       assert.equal(jStat.det(A), -360);
-    }
+    },
+    "test bug #270": function (jStat) {
+      var A = [
+        [310, -228, -190, 108],
+        [-228, 310, 108, -190],
+        [-190, 108, 310, -228],
+        [108, -190, -228, 310],
+      ];
+      assert.equal(jStat.det(A), 0);
+    },
   }
 });
 
