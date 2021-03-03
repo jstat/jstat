c08eaa956d63936e95700ebcc659a90ef7da01cc
Brice Leroy <brice@brigalabs.com>
2021-03-03 10:01:00 -0500

    fix statiscal_test test

--- types/statistical_test.test-d.ts
+++ types/statistical_test.test-d.ts
@@ -8,9 +8,9 @@ const sampleVector = [0, 1, 2];
 const sampleMatrix = [sampleVector, sampleVector, sampleVector];
 
 // zscore()
-expectType<number>(jStatCtx.zscore(2, sampleVector));
-expectType<number>(jStatCtx.zscore(2, sampleVector, true));
-expectType<number>(jStatCtx.zscore(2, 0, 1));
+expectType<number>(jStat.zscore(2, sampleVector));
+expectType<number>(jStat.zscore(2, sampleVector, true));
+expectType<number>(jStat.zscore(2, 0, 1));
 expectType<number>(jStat(sampleMatrix).zscore(2));
 expectType<number>(jStat(sampleMatrix).zscore(2, true));
 
@@ -21,10 +21,10 @@ expectType<number>(jStat(sampleMatrix).zscore(2, true));
 // ttest()
 
 // anovafscore()
-expectType<number>(jStatCtx.anovafscore(sampleMatrix));
-expectType<number>(jStatCtx.anovafscore(sampleVector, sampleVector));
+expectType<number>(jStat.anovafscore(sampleMatrix));
+expectType<number>(jStat.anovafscore(sampleVector, sampleVector));
 
 // anovaftest()
-expectType<number>(jStatCtx.anovaftest(sampleVector, sampleVector));
+expectType<number>(jStat.anovaftest(sampleVector, sampleVector));
 
-expectType<jStatCtx.TukeyHSD[]>(jStatCtx.tukeyhsd(sampleMatrix));
+expectType<jStat.TukeyHSD[]>(jStat.tukeyhsd(sampleMatrix));
